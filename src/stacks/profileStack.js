14abc4f5d18f1337e567d01c87fd2d528ecc3ce2
Damien Drozd <damien.drozd@icloud.com>
2023-04-17 09:33:32 +0200

    ajout de la page profile film

--- src/stacks/profileStack.js
+++ src/stacks/profileStack.js
@@ -10,6 +10,7 @@ import Biographie from "../screens/profile/profile_biographie"
 import ProfileInterest from "../screens/profile/profile_interest"
 import Question from "../screens/profile/profile_question"
 import Location from "../screens/profile/profile_location"
+import Film from "../screens/profile/profile_film"
 
 import {GetUser} from "../functions/api_request"
 
@@ -40,8 +41,8 @@ const PublicStack = () => {
 
   return (
     <UserContext.Provider value={"user"}>
-      <Stack.Navigator initialRouteName="Profile1" screenOptions={{headerShown: false}} >
-        <Stack.Screen name="Profile1" component={Profile1} />
+      <Stack.Navigator initialRouteName="Film" screenOptions={{headerShown: false}} >
+        <Stack.Screen name="Film" component={Film} />
         <Stack.Screen name="Profile2" component={Profile2} />
         <Stack.Screen name="Profile3" component={Profile3} />
         <Stack.Screen name="Profile4" component={Biographie} />
