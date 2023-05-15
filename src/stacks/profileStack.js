2b4da38f6cdcf71a409b31a98b35823273636e4d
okabasele <ocekabasele@gmail.com>
2023-05-15 16:31:17 +0200

    update profile stack

--- src/stacks/profileStack.js
+++ src/stacks/profileStack.js
@@ -48,10 +48,10 @@ const PublicStack = () => {
         <Stack.Screen name="Profile3" component={Profile3} />
         <Stack.Screen name="Profile4" component={Biographie} />
         <Stack.Screen name="Profile5" component={Gif} />
-        <Stack.Screen name="Film" component={Film} />
-        <Stack.Screen name="Profile6" component={ProfileInterest} />
-        <Stack.Screen name="Profile7" component={Location} /> 
-        <Stack.Screen name="Profile8" component={Question} />
+        <Stack.Screen name="Profile6" component={Film} />
+        <Stack.Screen name="Profile7" component={ProfileInterest} />
+        <Stack.Screen name="Profile8" component={Location} /> 
+        <Stack.Screen name="Profile9" component={Question} />
       </Stack.Navigator>
     </UserContext.Provider>
   );
