75a90d26b4cf43943f16f3330b56e0c9d0838069
Damien Drozd <damien.drozd@icloud.com>
2023-05-19 14:57:42 +0200

    adding crashlitics and performance

--- src/functions/request_fetch.js
+++ src/functions/request_fetch.js
@@ -1,9 +1,9 @@
+import crashlytics from '@react-native-firebase/crashlytics';
 
 export const FetchPeelApi = async ({ url, method, body, token, firebaseToken }) => {
 
     try {
-        console.log(`${process.env.API_LINK}${url}`)
-        console.log("firebaseToken : ", firebaseToken)
+        crashlytics().log("\n\n FetchPeelApi")
         const response = await fetch(`${process.env.API_LINK}${url}`, {
             headers: {
             "Content-Type": "Application/json",
@@ -19,17 +19,18 @@ export const FetchPeelApi = async ({ url, method, body, token, firebaseToken }) 
             body:JSON.stringify(body)
             }
         });
+        console.log("dataJson : ", response);
         const dataJson = await response.json();
         let status_code = response.status;
         if (status_code !== 200) {
             throw new Error(dataJson.message);
         }
         dataJson.success = true;
-        console.log("dataJson : ", dataJson);
+        crashlytics().log("dataJson : ", dataJson);
         return dataJson;
     }
     catch (error) {
-        console.log("error : ", error)
+        crashlytics().recordError(error);
         return Promise.reject(error);
     }
 }
