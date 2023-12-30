afeb9fa9a3b7928f15ed73afae7c4f58e137fcc2
Tadayoshi123 <yankill@outlook.fr>
2023-12-30 15:02:42 +0100

    feat(splashscreen): added splashscreen for android users

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Routes from './src/config/routes';
-import { NavigationContainer } from '@react-navigation/native';
-import { View, StatusBar } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {View, StatusBar, Platform} from 'react-native';
 import FlashMessage from 'react-native-flash-message';
-import { ThemeProvider } from 'styled-components';
+import {ThemeProvider} from 'styled-components';
 import theme from './src/config/theme';
-import { messageLisner } from './src/functions/messaging';
+import {messageLisner} from './src/functions/messaging';
+import SplashScreen from 'react-native-splash-screen';
 
 //On mets les routes dans un composant "Routes"
 const App = () => {
+  
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      SplashScreen.hide();
+    }
+  }, []);
+
   messageLisner();
   return (
     <>
       <NavigationContainer>
         <ThemeProvider theme={theme}>
-          <View style={{ flex: 1 }}>
+          <View style={{flex: 1}}>
             <Routes />
             <FlashMessage position="top" />
           </View>
