3d110f818daba5ee52d50d1c79be6382c58804d1
KevOneRedOne <kevinalves@hotmail.fr>
2024-01-17 15:32:25 +0100

    Release 1.14

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -37,7 +37,8 @@ const Login = ({ navigation }) => {
         const email_regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
         if (!email.toLowerCase().match(email_regex)) {
             // alert("addresse email non valide");
-            alert(process.env.API_PATH);
+            alert(process.env.API_LINK);
+            
         } else {
             loginRequest(email, password, navigation).then(({ error, message }) => {
                 if (error) {
