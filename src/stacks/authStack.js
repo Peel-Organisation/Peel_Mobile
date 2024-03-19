26229a901fc12d48ea867330cb5429a1e9e587d2
DamienDrozd <damien.drozd@icloud.com>
2024-03-19 22:50:38 +0100

    fix(ios): profil and auth stacks

--- src/stacks/authStack.js
+++ src/stacks/authStack.js
@@ -32,7 +32,7 @@ const TabStackScreen = () => {
         tabBarShowLabel: false,
         tabBarLabel: () => null, 
         tabBarStyle: {
-          height: 60,
+          height: 65,
           borderTopWidth: 1,
           elevation: 1,
           shadowOpacity: 0,
