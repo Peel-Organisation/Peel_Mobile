38988726569b250d4e01cc8b59799bbe5c16514c
DamienDrozd <damien.drozd@icloud.com>
2024-03-19 22:48:52 +0100

    fix(ios): Filter component

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,36 +1,30 @@
 import FilterButton from "../UI/FilterButton";
 import FilterSelector from "../UI/FilterSelector";
-import { ButtonView, Container, Background, FilterIcon, FilterIconImg, FiltersView, Header, Search, TitleText } from "./styles";
+import { ButtonView, Container, FiltersView, Header, TitleText } from "./styles";
 import { useTranslation } from "react-i18next";
 
 
-const Filter = ({ filter, setFilter, filtersArray, setFiltersArray, activeFilters, setActiveFilters, FilterPage }) => {
+const Filter = ({ filter, setFilter, filtersArray, setFiltersArray, activeFilters, setActiveFilters }) => {
     const { t } = useTranslation();
 
     return (
-        <Background onPress={() => { setFilter(!filter) }} activeOpacity={1}>
-            <Container activeOpacity={1}>
-                <Header>
+        <Container >
+            <FiltersView activeOpacity={1}>
+                <Header >
                     <TitleText>{t("home.filter")}</TitleText>
-                    <FilterIcon onPress={() => { setFilter(!filter) }}>
-                        <FilterIconImg source={require('../../../assets/images/icons/sort.png')} />
-                    </FilterIcon>
                 </Header>
-                <FiltersView>
-                    <FilterSelector title={t('Filter.interest')} filterName="interest" filtersArray={filtersArray} setFiltersArray={setFiltersArray} />
-                    <FilterSelector title={t('Filter.music')} filterName="music" filtersArray={filtersArray} setFiltersArray={setFiltersArray} />
-                    <FilterSelector title={t('Filter.sport')} filterName="sport" filtersArray={filtersArray} setFiltersArray={setFiltersArray} />
-                    <FilterSelector title={t('Filter.movie')} filterName="movie" filtersArray={filtersArray} setFiltersArray={setFiltersArray} />
-                    <FilterSelector title={t('Filter.games')} filterName="games" filtersArray={filtersArray} setFiltersArray={setFiltersArray} />
-                    
-                    <ButtonView>
-                        <FilterButton title={t('Filter.cancel')} setActiveFilters={setActiveFilters} activeFilters={activeFilters} filtersArray={filtersArray} setFiltersArray={setFiltersArray} filter={filter} setFilter={setFilter} />
-                        <FilterButton primary={true} title={t('Filter.apply')} setActiveFilters={setActiveFilters} activeFilters={activeFilters} filtersArray={filtersArray} setFiltersArray={setFiltersArray} filter={filter} setFilter={setFilter} />
-                    </ButtonView>
-
-                </FiltersView>
-            </Container>
-        </Background>
+                <FilterSelector title={t('Filter.interest')} filterName="interest" filtersArray={filtersArray} setFiltersArray={setFiltersArray} />
+                <FilterSelector title={t('Filter.music')} filterName="music" filtersArray={filtersArray} setFiltersArray={setFiltersArray} />
+                <FilterSelector title={t('Filter.sport')} filterName="sport" filtersArray={filtersArray} setFiltersArray={setFiltersArray} />
+                <FilterSelector title={t('Filter.movie')} filterName="movie" filtersArray={filtersArray} setFiltersArray={setFiltersArray} />
+                <FilterSelector title={t('Filter.games')} filterName="games" filtersArray={filtersArray} setFiltersArray={setFiltersArray} />
+                
+                <ButtonView>
+                    <FilterButton title={t('Filter.cancel')} setActiveFilters={setActiveFilters} activeFilters={activeFilters} filtersArray={filtersArray} setFiltersArray={setFiltersArray} filter={filter} setFilter={setFilter} />
+                    <FilterButton primary={true} title={t('Filter.apply')} setActiveFilters={setActiveFilters} activeFilters={activeFilters} filtersArray={filtersArray} setFiltersArray={setFiltersArray} filter={filter} setFilter={setFilter} />
+                </ButtonView>
+            </FiltersView>
+        </Container>
     );
 };
 
