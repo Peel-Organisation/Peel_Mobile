73dc454406b85baf5e79a8bc9df2d302025fae9e
KevOneRedOne <kevinalves@hotmail.fr>
2024-04-16 20:52:24 +0200

    Add StatusBar component to App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Routes from './src/config/routes';
 import { NavigationContainer } from '@react-navigation/native';
-import { View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import FlashMessage from 'react-native-flash-message';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/config/theme';
@@ -12,6 +12,7 @@ const App = () => {
   messageLisner();
   return (
     <>
+      <StatusBar barStyle="light-content" backgroundColor="#FC912F" />
       <NavigationContainer>
         <ThemeProvider theme={theme}>
           <View style={{ flex: 1 }}>
